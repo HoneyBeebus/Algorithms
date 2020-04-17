3fc113047f3da033c08db68c5cdfef5d8ab662d4
Henry White (Macbook) <cwhite24@ucmerced.edu>
2020-04-16 22:49:02 -0700

    finished lab07 and started lab08

---
+++ Lab08/yourid.cpp
@@ -0,0 +1,19 @@
+//
+//  yourid.cpp
+//  
+//
+//  Created by Henry White on 15/04/2020.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int matrixChainOrder(int p[], int n) {
+    int m[n][n];
+    int s[n-1][n-1];
+    for (int i = 1; i < n; i++) {
+        m[i][i] = 0;
+    }
+}
